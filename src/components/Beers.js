67c2337531213f5fb40fe634b565d0efbba7b0dc
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-02-27 22:36:12 +0100

    styled a line between beers

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -24,13 +24,16 @@ export default class Beers extends Component {
         if (this.state.beers.length === 0) return <h3>Loading...</h3>
         let beersList = this.state.beers.map(beer => {
             return (
-                <div style={{width: '50vw'}} className="d-flex flex-row justify-content-center mx-auto mt-3 mb-3">
-                    <img style={{height: "28vh", width: "5vw"}} className="ml-5 mr-5" src={beer.image_url} alt=""/>
-                    <div style={{height: "28vh", width: "30vw"}} className="d-flex flex-column justify-content-center align-self-center">
-                        <Link to={`/beers/${beer._id}`} ><h1>{beer.name}</h1></Link>
-                        <h3>{beer.tagline}</h3>
-                        <p>Created by: {beer.contributed_by}</p>
+                <div>
+                    <div style={{width: '50vw'}} className="d-flex flex-row justify-content-center mx-auto mt-3 mb-3">
+                        <img style={{height: "28vh", width: "5vw"}} className="ml-5 mr-5" src={beer.image_url} alt=""/>
+                        <div style={{height: "28vh", width: "30vw"}} className="d-flex flex-column justify-content-center align-self-center">
+                            <Link to={`/beers/${beer._id}`} ><h1>{beer.name}</h1></Link>
+                            <h3>{beer.tagline}</h3>
+                            <p>Created by: {beer.contributed_by}</p>
+                        </div>
                     </div>
+                    <hr className="mx-auto mt-5 mb-3" style={{width:'30vw', height: '3vh', borderTop: "1px solid grey"}}></hr>
                 </div>
             )
         })
