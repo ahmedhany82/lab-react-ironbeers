db0e095eb078f46c27f6f7972ba88a19221d3e14
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-02-28 22:54:09 +0100

    Done with all iterations

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 export default class Beers extends Component {
 
     state = {
+        query: "",
         beers: []
     }
 
@@ -19,12 +20,25 @@ export default class Beers extends Component {
         })
     }
 
+    handleSearch = event => {
+        const query = event.target.value;
+        this.setState({
+            query: query
+        })
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${query}`).then(response => {
+            this.setState({
+                beers: response.data
+              })      
+        }).catch(err => {
+            console.log(err)
+        })
+      }
 
-    render() {
-        if (this.state.beers.length === 0) return <h3>Loading...</h3>
+
+    render() {       
         let beersList = this.state.beers.map(beer => {
             return (
-                <div>
+                <div key={beer._id}>
                     <div style={{width: '50vw'}} className="d-flex flex-row justify-content-center mx-auto mt-3 mb-3">
                         <img style={{height: "28vh", width: "5vw"}} className="ml-5 mr-5" src={beer.image_url} alt=""/>
                         <div style={{height: "28vh", width: "30vw"}} className="d-flex flex-column justify-content-center align-self-center">
@@ -40,6 +54,18 @@ export default class Beers extends Component {
         return (
             <div className="d-flex flex-column justify-content-center">
                 <Navbar/>
+                <form style={{width:'100%', marginBottom: '50px'}}>
+                <div className="form-group">
+                    <label className="ml-3" htmlFor="search">Find a Beer!</label>
+                    <input className="form-control" style={{width:'90%', height: '25px', marginLeft: '20px'}}
+                    type="text"
+                    name="search"
+                    id="search"
+                    value={this.state.query}
+                    onChange={this.handleSearch}
+                    />
+                </div>
+                </form>
                 {beersList}
             </div>
         )
